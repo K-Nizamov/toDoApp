81bccf1ce5d0d033622974b9b6f43a33b4a0bd20
Kerim Nizamov <kerimnizamov@Kerims-MacBook-Air.local>
2021-11-30 20:14:54 +0200

    Add searching funcionality

--- src/components/JobsContainer/JobsContainer.js
+++ src/components/JobsContainer/JobsContainer.js
@@ -1,32 +1,42 @@
 import { useEffect, useState } from 'react';
 import JobElement from './JobElement'
 
-function JobsContainer({ toDos, setToDos,setVisibility,currentLi,setCurrentLi}) {
+function JobsContainer({ toDos, setToDos, setVisibility, setCurrentLi }) {
 
-  const [sorted,setSorted] = useState([])
-    useEffect(()=>{
-        let sortedToDos = toDos.sort((a, b) => b.significance.localeCompare(a.significance))
+    const [filteredText, setFilteredText] = useState('')
+  
+    function searchFunction(e) {
+        e.preventDefault()
+        let filterText = e.target.value
+        setFilteredText(filterText)
+    }
+
+    const [sorted, setSorted] = useState([])
+    useEffect(() => {
+        let filteredArr = toDos.filter(obj => obj.job.toUpperCase().includes(filteredText.toUpperCase()))
+        let sortedToDos = filteredArr.sort((a, b) => b.significance.localeCompare(a.significance))
         setSorted(sortedToDos)
-    },[toDos])
+    }, [toDos,filteredText])
+
     return (
         <>
             <section className="to-do-section">
                 <article className="record-wrapper">
                     <h1 className="header">JOB LIST</h1>
-                    <input className="header" id="search" type="text" placeholder="Search Job" />
+                    <input className="header" id="search" type="text" onKeyUp={searchFunction} placeholder="Search Job" />
                 </article>
                 <article className="record-container" id="divContainer">
                     {sorted.map(x =>
                         <JobElement
-                            todo = {x}
+                            todo={x}
                             key={x.id}
-                            id = {x.id}
+                            id={x.id}
                             toDos={toDos}
                             name={x.job}
                             setToDos={setToDos}
                             significance={x.significance}
-                            setVisibility = {setVisibility}
-                            setCurrentLi = {setCurrentLi}
+                            setVisibility={setVisibility}
+                            setCurrentLi={setCurrentLi}
                         />)}
                 </article>
             </section>
